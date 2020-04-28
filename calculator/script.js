255865eca8225dd7645f1726ff1dcb47359dd911
Pawel Przebieda <pprzeb@gmail.com>
2020-04-28 12:33:00 +0200

    added keybord input, need to change calculation function

--- calculator/script.js
+++ calculator/script.js
@@ -2,13 +2,17 @@ const buttonsDigits = document.querySelectorAll('.digit');
 const buttonsCalc = document.querySelectorAll('.calculation');
 const input = document.getElementById('input');
 const resultbox = document.getElementById('result');
+document.addEventListener('keydown', (e) => {if (['.','0','1','2','3','4','5','6','7','8','9'].includes(e.key)) {
+    getValue()} else if(['+','-','*','/','%','Enter','Delete'].includes(e.key)) {calculation()}})
 
 
 
 let a = '';
 let b = '';
-let ifFirstValue = true;
+let isFirstValue = true;
 let resultboxtext = [''];
+let operation = '';
+let result = '';
 
 
 for (button of buttonsDigits) {
@@ -20,36 +24,89 @@ for (button of buttonsCalc) {
 }
 
 function getValue() {
-    console.log(event.target.value)
-    let value = event.target.value;
-    if (Number(value)>=0 && Number(value) <=9) {
-        if (ifFirstValue) {
-            a = a + value;
-            input.value = a;
+    let value;
+    if (event.type === 'click') {
+        value = event.target.value;
+        event.target.blur();
+    } else value = event.key;
+    
+    if (Number(value)>=0 && Number(value) <=9 || value === '.') {
+        if (isFirstValue) {
+            if (value !== '.') {
+                a = a + value;
+                input.textContent = a;
+            } else {
+                a ? a = a + value: a = '0' + value;
+                input.textContent = a;
+            }
         } else {
-            b = b + value;
-            input.value = b;
+            if (value !== '.') {
+                b = b + value;
+                input.textContent = b;
+            } else {
+                b ? b = b + value : b = '0' + value;
+                input.textContent = b;
+            }
         }
-    } 
+    } else if (value === 'plusminus') {
+        if (isFirstValue) {
+            if (Number(a) > 0) {
+                a = '-' + a;
+                input.textContent = a;
+            } else {
+                a = a.substring(1);
+                input.textContent = a;
+            }
+        } else {
+            if (Number(b) > 0) {
+                b = '-' + b;
+                input.textContent = b;
+            } else {
+                b = b.substring(1);
+                input.textContent = b;
+            }
+        }
+    }
 }
-let operation = '';
-let result = '';
+
+
 
 function calculation() {
-    let value = event.target.value;
-    if (value !== '=') {
+    
+    let value;
+    if (event.type === 'click') {
+        value = event.target.value
+    } else value = event.key;
+        
+    if (['+','-','*','/','%'].includes(value)) {
         operation = value;
-         if (ifFirstValue) {
+         if (isFirstValue) {
             resultboxtext.push(a);
             resultboxtext.push(operation); 
-            ifFirstValue = false;
-            input.value = '';   
-        } 
-    }
-
+            isFirstValue = false;
+            input.textContent = '';   
+        } else {
+            a = result;
     
-       
-    if (value === '=') {
+            if (resultboxtext[resultboxtext.length-2] === '=') {
+                resultboxtext.pop();
+                resultboxtext.pop();
+            }
+            
+            console.log('a' + a, resultboxtext);
+            resultboxtext.push(operation);
+            resultboxtext.push(b);
+            
+            
+            input.textContent = ''
+            b = '';
+
+            // resultboxtext.slice()
+        }
+    }
+  
+    if (value === '=' || value ==='Enter') {
+        console.log('here', value)
         if (operation === '+') {
             if (b !== '') {
                 result = Number(a) + Number(b);
@@ -89,28 +146,31 @@ function calculation() {
             let c = b/100;
             result = Number(a) * Number(c);
         }
-    } else {input.value = result}
-        
+    };
 
-    // input.value = result;
-    // resultboxtext.push(b);
-    // resultboxtext.push('=', result);
-    
+    if (value === 'ce' || value ==='Delete') {
+       return reset()
+    }   
+  
     updatetextBox();
     
 }
+
 function updatetextBox() {
     resultbox.textContent = resultboxtext.join(' ');
     console.log(resultboxtext);
+    
     
 }
 
 function reset() {
     a = '';
     b = '';
-    operation = '';
-    firstValue = true;
-    input.value = '0'
+    isFirstValue = true;
     resultboxtext = [''];
-    update();
+    operation = '';
+    result = '';
+    input.textContent = ''
+    event.target.blur()
+    updatetextBox();
 }
